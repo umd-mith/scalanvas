3a93bde5e0beee55218f0619dbf5300ab3213931
Travis Brown <travisrobertbrown@gmail.com>
2014-04-13 12:09:40 -0400

    Added extended configuration.

---
+++ extensions/src/main/scala/extensions/model/configuration.scala
@@ -0,0 +1,12 @@
+package edu.umd.mith.scalanvas.extensions.model
+
+import edu.umd.mith.scalanvas.model.Configuration
+import java.net.URI
+
+trait MithConfiguration extends Configuration {
+  def adjustDimensions(w: Int, h: Int): (Int, Int) = (w, h)
+  def constructManifestLabel(titleText: String): String
+  def constructManifestTitle(titleText: String): String
+  def constructReadingUri(id: String): URI
+}
+
