72b631f3e0c16515ad8c6751ad9c29b3e71f6fd4
raffazizzi <raffaeleviglianti@gmail.com>
2014-04-23 13:46:30 -0400

    jp2 urls

--- core/src/main/scala/sga/wwa/configuration.scala
+++ core/src/main/scala/sga/wwa/configuration.scala
@@ -15,7 +15,7 @@ trait WwaConfiguration { this: WwaManifest =>
 
   // The primary facsimile images are JPEG 2000 files served from Djatoka.
   // This value will need to be overriden for the static fallback manifests.
-  def imageFormat: String = "image/jpg"
+  def imageFormat: String = "image/jp2"
 
   // The image dimensions in the TEI files are based on the full TIFF images
   // that were delivered to MITH last year, which include a 200 pixel footer
@@ -26,7 +26,7 @@ trait WwaConfiguration { this: WwaManifest =>
   // This should be the identifier for any Djatoka server, so I'm including
   // it here as a default, but it will need to be overridden in some cases.
   def constructImageUri(idWithSeq: String) = new URI(
-    "/demo/images/wwa/%s.jpg".format(idWithSeq)
+    "http://spacely.unl.edu/waimages/%s".format(idWithSeq)
   )
 }
 
@@ -37,7 +37,7 @@ trait DevelopmentConfiguration { this: WwaConfiguration =>
 trait BodleianImages { this: WwaConfiguration =>
   def imageService = Some(
     Service(
-      new URI("http://tiles2.bodleian.ox.ac.uk:8080/adore-djatoka/resolver"),
+      new URI("http://spacely.unl.edu:8080/adore-djatoka/resolver"),
       Some(new URI("http://sourceforge.net/projects/djatoka/"))
     )
   )
@@ -52,7 +52,7 @@ trait MithDjatokaImages { this: WwaConfiguration =>
   )
 
   override def constructImageUri(idWithSeq: String) = new URI(
-    "/demo/images/wwa/%s.jpg".format(idWithSeq)
+    "%s".format(idWithSeq)
   )
 
   override def adjustDimensions(w: Int, h: Int): (Int, Int) = (w, h)
